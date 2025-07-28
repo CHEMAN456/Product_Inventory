75d78c40b1d3bca607b25a0c790a7603a0466e38
chetan mandalkar <you@example.com>
2025-07-29 01:31:54 +0530

    Redirect to inventory:index

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import home_view,index_view,create_product,product_list,create_transaction,inventory_status,transaction_list,ProductViewSet,TransactionViewSet
@@ -13,6 +14,7 @@ router.register(r'transactions', TransactionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('inventory:index')),   
     path('home/', home_view, name='home'),
     path('index/', index_view, name='index'),
     path('home/add_product/',create_product , name='add_product'),
@@ -20,5 +22,6 @@ urlpatterns = [
     path('home/create_transaction/',create_transaction, name='create_transaction'),
     path('home/inventory/',inventory_status , name='inventory_list'),
     path('home/transaction/',transaction_list , name='trans_list'),
-    path('api/', include(router.urls))      
+    path('api/', include(router.urls)),
+       
 ]
